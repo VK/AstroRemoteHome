20d10d7e4b367ef2d0c734e0c8af29da235fd1ed
Viktor Krückl <Viktor.Krueckl@osram.com>
2020-01-04 20:19:28 +0100

    initial commit

---
+++ precache-manifest.e465314fbb53011e61c3d12737fafdcd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "b187eb2fa10faad12e1f19ade088fafd",
+    "url": "/AstroRemoteHome/img/setup.png"
+  },
+  {
+    "revision": "a0c9f5b22be1f87b6fca84d7e6fe9dc0",
+    "url": "/AstroRemoteHome/img/app.gif"
+  },
+  {
+    "revision": "3675249eaf9f04f4215353d00f879178",
+    "url": "/AstroRemoteHome/img/box_open.png"
+  },
+  {
+    "revision": "f650f75a2a594c02df20",
+    "url": "/AstroRemoteHome/js/chunk-vendors.4cfde9b7.js"
+  },
+  {
+    "revision": "9cd1417ccd3ff863f07295026882f6bd",
+    "url": "/AstroRemoteHome/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/AstroRemoteHome/robots.txt"
+  },
+  {
+    "revision": "508f1c92b12a8a87ab39",
+    "url": "/AstroRemoteHome/js/app.650bf5c7.js"
+  },
+  {
+    "revision": "9ef3e01e88951fdba254de2867f26d0d",
+    "url": "/AstroRemoteHome/img/breadboard.png"
+  },
+  {
+    "revision": "a56d3097ac61ec496fa17ebc8d597623",
+    "url": "/AstroRemoteHome/img/blurred_logo.png"
+  },
+  {
+    "revision": "c7541093745c285f9e6d67b9ec8e0978",
+    "url": "/AstroRemoteHome/img/box_closed.png"
+  },
+  {
+    "revision": "f650f75a2a594c02df20",
+    "url": "/AstroRemoteHome/css/chunk-vendors.520e1ef3.css"
+  },
+  {
+    "revision": "508f1c92b12a8a87ab39",
+    "url": "/AstroRemoteHome/css/app.64b5444c.css"
+  }
+];
